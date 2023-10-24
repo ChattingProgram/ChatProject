b238ecc0f6ba425c4a1922d772f4237af240a119
mohyeokjin <ddubi1115@naver.com>
2023-10-24 11:05:53 +0900

    메뉴 수정

--- Client/menu.cpp
+++ Client/menu.cpp
@@ -50,7 +50,7 @@ void MainMenu() {
     cout << "■              *****      ****    *     *               ■\n";
     cout << "■                                                       ■\n";
     cout << "■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■\n\n";
-    cout << "        조작방법 W A S D / 결정 : 스페이스바   \n";
+    cout << "          조작방법 W A S D / 결정 : 스페이스바   \n";
 
     //getchar(); // 아무키 입력 기다림
     //system("cls"); // 콘솔 창 clear
@@ -69,17 +69,17 @@ int MenuDraw() { //첫 게임 시작화면
     // 위쪽 : y감소
     // 
      // 메뉴 코드 0:로그인 / 1:id찾기 / 2:pw찾기 / 3:회원가입 / 4:대화 / 5:기존채팅 / 6:친추 / 7:비번수정
-    int x = 24;
+    int x = 21;
     int y = 24;
-    gotoxy(x - 2, y); // 23 , 24
+    gotoxy(x - 2, y); // 20 , 24
     printf(">  로그인  "); //다른 애들보다 x값이 작은건 > 때문에 자연스럽게 보일려고
-    gotoxy(x, y + 1); // 24 , 25
+    gotoxy(x, y + 1); // 21 , 25
     printf(" ID 찾기 ");
-    gotoxy(x, y + 2); // 24, 26
+    gotoxy(x, y + 2); // 21, 26
     printf(" PW 찾기 ");
-    gotoxy(x, y + 3); // 24, 27
+    gotoxy(x, y + 3); // 21, 27
     printf(" 회원 가입 ");
-    gotoxy(x, y + 4); // 24, 28
+    gotoxy(x, y + 4); // 21, 28
     printf(" 프로그램 종료 ");
 
     while (1) {
@@ -123,17 +123,17 @@ int Login_MenuDraw() { //첫 게임 시작화면
     // 아래 : y증가
     // 위쪽 : y감소
 
-    int x = 24;
+    int x = 21;
     int y = 24;
-    gotoxy(x - 2, y); // 23 , 24
+    gotoxy(x - 2, y); // 20 , 24
     printf(">  대화하기  "); //다른 애들보다 x값이 작은건 > 때문에 자연스럽게 보일려고
-    gotoxy(x, y + 1); // 24 , 25
+    gotoxy(x, y + 1); // 21 , 25
     printf(" 기존 대화방 ");
-    gotoxy(x, y + 2); // 24, 26
+    gotoxy(x, y + 2); // 21, 26
     printf(" 친구 추가 ");
-    gotoxy(x, y + 3); // 24, 27
+    gotoxy(x, y + 3); // 21, 27
     printf(" 비밀번호 수정 ");
-    gotoxy(x, y + 4); // 24, 28
+    gotoxy(x, y + 4); // 21, 28
     printf(" 프로그램 종료 ");
 
     while (1) {
@@ -159,48 +159,6 @@ int Login_MenuDraw() { //첫 게임 시작화면
         }
         case SUBMIT: {
             return y - 24; // 시작 위치를 빼서 메뉴 선택을 3, 4, 5 값으로 좁힘.
-        }
-        }//스위치문 닫는 곳
-
-    }
-}
-
-
-int Gaming_select() { //게임 진행 중 선택하는 곳
-    int x = 3;
-    int y = 13;
-    gotoxy(x - 2, y);
-    printf(">  총기 획득 "); // 인덱스 0 반환!!!! 다른 애들보다 x값이 작은건 > 때문에 자연스럽게 보일려고
-    gotoxy(x, y + 1); // 36 , 16
-    printf(" 칼칼 획득 "); // 인덱스 1 반환!!!!
-    gotoxy(x, y + 2); // 36, 17
-    printf(" 공격 하기"); // 인덱스 2 반환!!!!
-    gotoxy(x, y + 3); // 36, 17
-    printf(" 종료 하기"); // 인덱스 3 반환!!!!
-
-    while (1) {
-        int n = keyControl(); //키값 받기
-        switch (n) {
-        case UP: {
-            if (y > 13) {
-                gotoxy(x - 2, y);
-                printf(" ");
-                gotoxy(x - 2, --y);
-                printf(">");
-            }
-            break;
-        }
-        case DOWN: {
-            if (y < 16) {
-                gotoxy(x - 2, y);
-                printf(" ");
-                gotoxy(x - 2, ++y);
-                printf(">");
-            }
-            break;
-        }
-        case SUBMIT: {
-            return y - 13; // 시작 위치를 빼서 메뉴 선택을 0 , 1 , 2 값으로 좁힘.
         }
         }//스위치문 닫는 곳
 
